31d61d68ff4356693eb4d7bc7ad684fac5f0119f
jade <zyyjade@gmail.com>
2013-09-04 23:23:47 +0800

    commit

--- static/js/mobile/mobile-default.js
+++
@@ -1,7 +0,0 @@
-$(document).bind("mobileinit", function(){
-  $.extend(  $.mobile , {
-    loadingMessage: "加载中,请耐心等待...",
-    pageLoadErrorMessage:"对不起，页面加载失败，请稍后再试。",
-    defaultPageTransition:'slide'
-  });
-});
